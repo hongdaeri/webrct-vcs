651af7149c4dce68ea00260c306a706314f3ef26
홍바울 <hongp0508@gmail.com>
2021-01-07 13:40:39 +0900

    asd

--- src/socketController.js
+++ src/socketController.js
@@ -1,4 +1,15 @@
 
+function getParam(sname) {
+    let params = location.search.substr(location.search.indexOf("?") + 1);
+    let sval = "";
+    params = params.split("&");
+    for (let i = 0; i < params.length; i++) {
+        temp = params[i].split("=");
+        if ([temp[0]] == sname) { sval = temp[1]; }
+    }
+    return sval;
+}
+
 peers = {}
 
 
