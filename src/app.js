bb80783a7428a990eba5c89a9d6801741da5175a
홍바울 <hongp0508@gmail.com>
2021-01-07 13:57:31 +0900

    a

--- src/app.js
+++ src/app.js
@@ -19,6 +19,13 @@ const port = process.env.PORT || 3012
 
 require('./routes')(app)
 
+app.get('/test', function(req, res) {
+    var user_id = req.param('id');
+    var user_name = req.param('name'); 
+   
+    res.send(user_id + ' ' + user_name);
+});
+
 const httpsServer = httpolyglot.createServer(options, app)
 const io = require('socket.io')(httpsServer)
 require('./socketController')(io)
@@ -28,12 +35,7 @@ httpsServer.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
 
-httpsServer.get('/test', function(req, res) {
-    var user_id = req.param('id');
-    var user_name = req.param('name'); 
-   
-    res.send(user_id + ' ' + user_name);
-});
+
   
   
 
