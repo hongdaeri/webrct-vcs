e89d60963d6dcbaa12b43e255d6069aacb5f9d08
홍바울 <hongp0508@gmail.com>
2021-01-22 18:23:25 +0900

    1

--- public/js/main.js
+++ public/js/main.js
@@ -424,4 +424,39 @@ function setVideoFilter(filter) {
    localVideo.className = filter;
 }
 
+const screenHandler = new ScreenHandler();
 
+  /**
+   * 비디오 엘리먼트에 재생을 위해 stream 바인딩
+   * @param data
+   */
+  function setVideoStream(data) {
+    const video = data.el;
+    video.srcObject = data.stream;
+  }
+
+
+ /**
+   * 로컬 스트림 핸들링
+   * @param stream
+   */
+  function onLocalStream(stream) {
+    console.log('onLocalStream', stream);
+
+    setVideoStream({
+      el: document.querySelector('#localVideo'),
+      stream: stream,
+    });
+  }
+
+ /**
+   * screenHandler를 통해 캡쳐 API 호출
+   */
+  function startScreenShare() {
+    screenHandler.start((stream) => {
+      onLocalStream(stream);
+    });
+  }
+
+
+ 
