d68440b4e8f53bbf4ba759f005414773ee4f3455
홍바울 <hongp0508@gmail.com>
2021-02-10 15:45:19 +0900

    123

--- public/js/main.js
+++ public/js/main.js
@@ -44,10 +44,17 @@ const configuration = {
         // public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
         // set your own servers here
         {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
+        }
+        /*
+        {
             url: 'turn:192.158.29.39:3478?transport=udp',
             credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
             username: '28224511:1379330808'
         }
+        */
     ]
 }
 
