3506dd613f2bada6a373d0ad4c688bae1e61f968
홍바울 <hongp0508@gmail.com>
2023-01-16 16:08:50 +0900

    g

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const options = {
     //cert: fs.readFileSync(path.join(__dirname,'..','ssl','cert.pem'), 'utf-8')
 }
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 443
 
 ////////////////////////////
 
